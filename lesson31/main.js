9cdba439f870a25efc9d5e93333e8fdeb177a279
yuliia-zinchenko <74060254+yuliia-zinchenko@users.noreply.github.com>
2021-02-22 17:51:40 +0200

    Add files via upload

---
+++ lesson31/main.js
@@ -0,0 +1,26 @@
+let x = Number(prompt("Введите x"))
+    y = Number(prompt("Введите y"))
+    z = (x + y)
+console.log(x+y)
+console.log(x-y)
+console.log(x*y)
+console.log(x/y)
+console.log(x**y)
+console.log(y%x)
+x++
+console.log(x)
+y--
+console.log(y)
+x+=7
+console.log(x)
+y-=8
+console.log(y)
+x*=3
+console.log(x)
+y/=2
+console.log(y)
+console.log(x>y)
+console.log(x<y)
+console.log(x==y)
+console.log(x >= y)
+alert("Сума " + z)
---
+++ lesson32/main.js
@@ -0,0 +1,56 @@
+//let x = " " + 1 + 0
+//console.log(x)
+//let y = true + false
+//console.log(y)
+//let a = 6/"3"
+//console.log(a)
+//let b = 4 + 5 + "px"
+//console.log(b)
+//let c = "px" + 5 + 4
+//console.log(c)
+//let d  = "4" - 2
+//{console.log(d)
+//let e = Number(prompt("Введите первое число "))
+//  f = Number(prompt("Введите второе число "))
+//alert(e+f)
+//console.log(5>4)
+//console.log("ананас">"яблоко")
+//console.log("2">"12")
+//console.log("Roma"<"Romanuch")
+//let g = prompt("a= ")
+//  h = prompt("b= ")
+//g++
+//g+=b
+//alert(g>h)}
+
+//let  a = prompt("Какое число?")
+//    if(a==10){
+//        alert("Молодец")
+//    }
+//    else if (a==7){
+//        alert("Молодец")
+//    }
+//    else {
+//        alert("Не молодец")
+//    }
+//let a = prompt("Введите свой возраст")
+//    if (a<6){
+//        alert("Дошкольник")
+//    }
+//    else if (a<18){
+//        alert("Школьник")
+//    }
+//    else if (a>=18) {
+//        alert("Взрослый")
+//}
+
+let a = prompt("В каком году был создан первый сценарий JS?")
+    if (a<2015) {
+        alert("Вы неправы, немного больше")
+    }
+    else if (a>2015) {
+        alert("Вы неправы, немного меньше")
+    }
+    else if (a=2015) {
+        alert("Правильно!")
+    }
